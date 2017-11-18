ba5e0743a693e0ffc718f31ae83a1e23e1bd6b90
Kirill Polyanin <k.polianin@gmail.com>
2017-11-18 04:16:11 +0200

    test commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send({ movie: 'Thor: Ragnaroc' });
+	res.send({ movie: 'Thor' });
 });
 
 const PORT = process.env.PORT || 5000;
